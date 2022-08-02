bed4e5396272bd1026df7896eba60b3d4e69a5bc
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-08-02 10:24:49 -0400

    Minor update to second script.

--- R/2_merge_NAPS_data.R
+++ R/2_merge_NAPS_data.R
@@ -1,10 +1,10 @@
-# 2_merge_NAPS_data.R
+# 2_merge_naps_data.R
 
 
 # Project : pollution_air_limoilou
 # Author  : Jeremie Boudreault
-# Email   : jeremie [dot] bodreault [at] inrs [dot] ca
-# Depends : R (v4.1.2)
+# Email   : Prenom.Nom@inrs.ca
+# Depends : R (v4.2.1)
 # License : None
 
 
@@ -14,19 +14,16 @@
 library(data.table)
 
 
-# Parameters -------------------------------------------------------------------
+# Load all downloaded files ----------------------------------------------------
 
 
-years   <- seq.int(2000L, 2019L)
-pols    <- c("O3", "PM25", "PM10", "CO", "NO2", "SO2")
-choices <- expand.grid(years, pols)
+# List files.
+files_path <- file.path("data", "naps", "raw")
+files <- list.files(files_path)
 
-
-# Load all files ---------------------------------------------------------------
-
-
+# Load all files.
 pols_list <- lapply(
-    X    = paste0("data/NAPS/", choices[[2L]], "_", choices[[1L]], ".csv"),
+    X    = file.path(files_path, files),
     FUN  = data.table::fread,
     skip = 5L
 )
@@ -38,12 +35,13 @@ pols_list <- lapply(
 # Note : Some dataset have 32 columns, other have 31. Names differ from datasets,
 #        but the information is the same. Here, we harmonize all datasets.
 
+
 # For datasets with 32 columns.
 which_lines_32 <- which(lengths(pols_list) == 32L)
 pols_list[which_lines_32] <- lapply(
     X   = pols_list[which_lines_32],
     FUN = function(x) {
-        names(x) <- names(pols_list[[21L]])
+        names(x) <- names(pols_list[[which_lines_32[1L]]])
         x
     }
 )
@@ -53,11 +51,15 @@ which_lines_31 <- which(lengths(pols_list) == 31L)
 pols_list[which_lines_31] <- lapply(
     X   = pols_list[which_lines_31],
     FUN = function(x) {
-        names(x) <- names(pols_list[[1]])
+        names(x) <- names(pols_list[[which_lines_31[1L]]])
         x$Method <- NA
         x
     }
 )
+
+# Final checks.
+table(lengths(pols_list))
+data.frame(table(unlist(lapply(pols_list, names))))
 
 
 # Fix dates --------------------------------------------------------------------
@@ -83,7 +85,7 @@ pols_list <- lapply(
 # Merge.
 pols_dt <- data.table::rbindlist(pols_list, use.names = TRUE)
 
-# Replace 999 with NAs.
+# Replace -999 with NAs.
 cols <- c(paste0("H0", 1:9), paste0("H", 10:24))
 
 # Replace -999 by NA.
@@ -98,8 +100,9 @@ for (i in seq.int(1L, 24L)) {
 }
 
 
-# Export.
-qs::qsave(
-    pols_dt, file.path("data", "NAPS_cleaned.qs")
-)
+# Export -----------------------------------------------------------------------
 
+
+qs::qsave(
+    pols_dt, file.path("data", "naps", "naps_cleaned.qs")
+)
